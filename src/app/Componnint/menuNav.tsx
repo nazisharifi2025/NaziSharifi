002a6dafae89e7f6168a938e80e585369c0e84d6
nazi sharifi <banonazisharifi@gmail.com>
2026-02-20 12:10:03 +0430

    add completed menu

--- src/app/Componnint/menuNav.tsx
+++ src/app/Componnint/menuNav.tsx
@@ -1,3 +1,5 @@
+import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger } from '@/components/ui/sheet';
+import { EllipsisVertical, MenuIcon } from 'lucide-react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import React from 'react'
@@ -37,8 +39,9 @@ function Menunav() {
     ]
     const PathName = usePathname();
   return (
-    <div>
-         <ul className='flex gap-5 text-xl text-stone-400'>
+    <div className='flex justify-end gap-3'>
+        <nav className='hidden lg:flex   gap-1'>
+         <ul className='flex gap-5 items-center text-xl text-stone-400'>
           {Navbar.map((nav)=>{
             const isActive = PathName === nav.href || (PathName.startsWith('nav.href') && nav.href !== '/');
             return(
@@ -49,6 +52,30 @@ function Menunav() {
               })}
           </ul>
         <button className='px-4 py-2 border-white text-white border rounded-xl font-bold'><Link href='#contactme'>Let's Taik</Link></button>
+        </nav>
+        <nav className='lg:hidden'>
+            <Sheet >
+                <SheetTrigger className='flex items-center space-x-2'>
+                    <button className='px-4 py-2 border-white text-white border hidden md:flex rounded-xl font-bold'><Link href='#contactme'>Let's Taik</Link></button>
+                    <MenuIcon className='text-white' size={32} />
+                </SheetTrigger>
+                <SheetContent className='p-4'>
+                    <SheetTitle>Menu</SheetTitle>
+            <ul className='flex flex-col gap-5 p-4 text-xl text-stone-400'>
+          {Navbar.map((nav)=>{
+            const isActive = PathName === nav.href || (PathName.startsWith('nav.href') && nav.href !== '/');
+            return(
+          <li key={nav.id} className='hover:border-b-2 border-fuchsia-400 hover:text-white transition-all duration-500 '>
+            <Link className={isActive? "text-fuchsia-400 , font-bold" : ""} href={nav.href}>{nav.Name}</Link>
+          </li>
+            )
+              })}
+          </ul>
+          <button className='px-4 py-2 border-gray-600 w-fit text-gray-600 border rounded-xl font-bold'><Link href='#contactme'>Let's Taik</Link></button>
+        <SheetDescription></SheetDescription>
+                </SheetContent>
+            </Sheet>
+        </nav>
     </div>
   )
 }
