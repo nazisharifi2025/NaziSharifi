1c1cf8338617f522dd9a87f409d05efcf1803f10
nazi sharifi <banonazisharifi@gmail.com>
2026-02-21 17:16:55 +0430

    add nw chenges

--- src/app/Componnint/Navbar.tsx
+++ src/app/Componnint/Navbar.tsx
@@ -5,7 +5,7 @@ function Navbar() {
    
   return (
     <div className="w-full flex justify-center fixed  top-3 z-50 items-center">
-    <div className='md:w-[60%] w-10/12 border border-stone-400 mx-auto h-15 py-7 px-3 rounded-md   bg-stone-400/20 flex items-center justify-between'>
+    <div className='md:w-[60%] w-10/12 border border-stone-400 mx-auto h-15 py-7 px-3 rounded-md  backdrop-blur-md  bg-stone-400/20 flex items-center justify-between'>
         <h1 className=" font-bold text-3xl font-serif bg-linear-to-l from-blue-300  to-blue-900 text-transparent  bg-clip-text">Nazi</h1>
        {/* menu */}
        <Menunav />
